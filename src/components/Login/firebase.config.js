5fa6ef99cf1cfa0eb299b165f73acb0bca58c48e
Rasel5267 <mahadiul09@gmail.com>
2020-09-30 11:35:18 +0600

    database issue

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtw-cHIGgqNtgrq9mzyQAYgYuiiUuTbXA",
+    authDomain: "burj-al-arab-authentication.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-authentication.firebaseio.com",
+    projectId: "burj-al-arab-authentication",
+    storageBucket: "burj-al-arab-authentication.appspot.com",
+    messagingSenderId: "84449484837",
+    appId: "1:84449484837:web:f424674b800856215b1fcc",
+    measurementId: "G-LT6ZQ5H9EF"
+  };
+
+export default firebaseConfig;
